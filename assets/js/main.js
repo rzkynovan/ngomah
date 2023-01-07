743d71a674619569f704a89b55deee97f573bccc
rzkynovan <rizkynovan21@gmail.com>
2023-01-07 22:14:28 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,23 @@ var swiper = new Swiper(".popularslide", {
         }
     },
   });
+
+  /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 100;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
